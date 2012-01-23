f022c893f2cdef4ad108fc58e09f250a8d7b37c7
Alain Holzhauser <alain.holzhauser@mmp.htwchur.ch>
2012-01-23 23:29:11 +0100

    naja

---
+++ app.js
@@ -0,0 +1,79 @@
+// 78.104.171.255
+
+var express = require('express');
+
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+  , clientSocket;
+  
+app.listen(3000);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+// build up socket for communicating between server and html
+io.sockets.on('connection', function (socket) {
+    clientSocket = socket;
+    
+    // socket for communicating between server and html
+    socket.on('serverSocket', function (data) {
+          console.log(data);
+          sendUpdPaketOutside(data);
+    });
+});
+  
+  
+var dgram = require("dgram");
+var server = dgram.createSocket("udp4");
+
+// socket for communicating with outer world
+server.on("message", function (msg, rinfo) {  
+  console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
+  clientSocket.emit('news', String(msg));
+});
+server.bind(1234);
+
+
+function sendUpdPaketOutside(data){
+
+  var message = new Buffer(String(data));
+  var socketOutside = dgram.createSocket("udp4");
+  
+  socketOutside.send(message, 0, message.length, 1234, "78.104.171.255", function(err, bytes) {
+    socketOutside.close();
+  });
+  
+ //listening on socket 
+ function socketListenOutside () {
+	 
+	  var message = new Buffer(String(data));
+	  
+ 		 var socketlisten = dgram.createSocket("udp4");
+  
+	 var echolot = dgram.listening("udp4");
+	 
+	 console.log(echolot);
+	 
+	 
+	 };
+}
+
